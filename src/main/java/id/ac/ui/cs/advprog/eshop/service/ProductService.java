b2a3b3ba3511228160526067554d9912743572fa
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-02-14 01:33:06 +0700

    delete unused modifiers

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public Product edit(Product product);
-    public Product findById(String id);
-    public Product deleteById(String id);
-    public List<Product> findAll();
+    Product create(Product product);
+    Product edit(Product product);
+    Product findById(String id);
+    Product deleteById(String id);
+    List<Product> findAll();
 }
