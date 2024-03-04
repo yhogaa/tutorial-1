b675b1a1f003847321ee315b59e9523a14d91017
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-03-04 15:44:35 +0700

    [GREEN] Implement OrderRepository class

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class OrderServiceImplTest {
+class OrderServiceImplTest {
     @InjectMocks
     OrderServiceImpl orderService;
     @Mock
