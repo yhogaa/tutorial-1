9b986fe7835906a5ed8c5ec032a0ee775c2b4c4b
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2024-03-06 19:21:34 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,14 +11,33 @@ public class PaymentRepository {
     private List<Payment> payments = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null;
+        int i = 0;
+        for (Payment savedPayment : payments) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                payments.remove(i);
+                payments.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+        payments.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
+        for (Payment savedPayment : payments) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public List<Payment> getAllPayment() {
-        return null;
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : payments) {
+            result.add(savedPayment);
+        }
+        return result;
     }
 }
